2b64e58f44521de685fc52d124f0530497fec5d4
Nikappa57 <lorenz.gaudino@gmail.com>
2023-05-10 12:27:14 +0200

    admin fix

--- Site/__init__.py
+++ Site/__init__.py
@@ -35,23 +35,23 @@ with app.app_context():
     else:
         migrate.init_app(app, db)
 
-from Site.models.permission import Permission
-from Site.models.users import Users
-from Site.models.section import Section
-from Site.models.category import Category
-from Site.models.threads_view import ThreadsViews
-from Site.models.threads import Threads
-from Site.models.cmt_upvote import CmtUpvote
-from Site.models.comment import Comments
-from Site.models.sub_upvote import SubUpvote
-from Site.models.subcomment import Subcomments
-if not "db" in sys.argv[1]:
-    from Site.views.Admin import routes
-    from Site.views.Main import routes
-    from Site.views.Forum import routes
-    from Site.views.Errors import routes
-    from Site.views.Other import routes
-    from Site.src.permission_default import create_default_permission
-    from Site.src.rank_default import create_default_ranks
-    create_default_permission()
-    create_default_ranks()
+    from Site.models.permission import Permission
+    from Site.models.users import Users
+    from Site.models.section import Section
+    from Site.models.category import Category
+    from Site.models.threads_view import ThreadsViews
+    from Site.models.threads import Threads
+    from Site.models.cmt_upvote import CmtUpvote
+    from Site.models.comment import Comments
+    from Site.models.sub_upvote import SubUpvote
+    from Site.models.subcomment import Subcomments
+    if not "db" in sys.argv[1]:
+        from Site.views.Admin import routes
+        from Site.views.Main import routes
+        from Site.views.Forum import routes
+        from Site.views.Errors import routes
+        from Site.views.Other import routes
+        from Site.src.permission_default import create_default_permission
+        from Site.src.rank_default import create_default_ranks
+        create_default_permission()
+        create_default_ranks()
